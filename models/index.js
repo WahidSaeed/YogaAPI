acc57145a38f9bb0dae30a398e47adef4349a8dd
Abdul Wahid <Abdul.Wahid@trakker.com.pk>
2020-04-05 23:28:52 +0500

    New Update

--- models/index.js
+++ models/index.js
@@ -10,18 +10,22 @@ const db = {};
 
 let sequelize;
 //if (config.use_env_variable) {
-  sequelize = new Sequelize("mysql://b31ab78da2682d:504561ed@us-cdbr-iron-east-04.cleardb.net/heroku_7b48086a6fe2b89?reconnect=true", {
-    database: "heroku_7b48086a6fe2b89",
-    username: "b31ab78da2682d",
-    password: "504561ed",
-    dialect:  "mysql",
-    protocol: "mysql",
-    port: "3306",
-    host: "us-cdbr-iron-east-04.cleardb.net",
-    logging:  true
-  });
+  // sequelize = new Sequelize("mysql://b31ab78da2682d:504561ed@us-cdbr-iron-east-04.cleardb.net/heroku_7b48086a6fe2b89?reconnect=true", {
+  //   database: "heroku_7b48086a6fe2b89",
+  //   username: "b31ab78da2682d",
+  //   password: "504561ed",
+  //   dialect:  "mysql",
+  //   protocol: "mysql",
+  //   port: "3306",
+  //   use_env_variable: "mysql://b31ab78da2682d:504561ed@us-cdbr-iron-east-04.cleardb.net/heroku_7b48086a6fe2b89?reconnect=true"
+  // });
 //} else {
-//  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize('pranayamauat', 'admin', 'admin', {
+      dialect:  "mysql",
+      protocol: "mysql",
+      port: "3306",
+      host: "us-mm-dca-3b5e2a917698.g5.cleardb.net"
+  });
 //}
 
 fs
